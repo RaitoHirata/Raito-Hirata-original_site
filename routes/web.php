0fae6176d420c83b5b6391d64bfac4d46c8089e4
RaitoHirata <right.1279@icloud.com>
2023-02-01 02:51:28 +0900

    ーティング記述をver8.1仕様に変更

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\Twittercontroller;
 use Illuminate\Support\Facades\Route;
 use App\Providers\AppServiceProvider;
 use App\Models\score;
@@ -26,53 +28,53 @@ Route::get('/', function () {
 })->name('index');
 
 
-Route::post('/','Contactcontroller@search')->name('search');
-Route::get('/score','Contactcontroller@scorelink')->name('scorelink');
+Route::post('/', [Contactcontroller::class, 'search'])->name('search');
+Route::get('/score',[ContactController::class, 'scorelink'])->name('scorelink');
 
 Route::group(['middleware'=>['auth','can:admin-higher']],function(){
     //manager
-    Route::get('/mng_register','Contactcontroller@mngregister')->name('mngregister');
-   // Route::get('/manager_login','Contactcontroller@managerLogin')->name('manager_login');
-   // Route::get('/manager_home','Contactcontroller@managerHome')->name('home');
-   // Route::get('/login/search','Contactcontroller@loginSearch')->name('login_search');
-   // Route::post('/manager_home','Contactcontroller@mngloginComplete')->name('home');
-    Route::get('/home/scoredata','Contactcontroller@scoreData')->name('scoredata');
-    Route::get('/manager_home/scoredata_delete','Contactcontroller@delete')->name('scoredelete');
-    Route::get('/manager_home/scoredata/scoredata_release','Contactcontroller@release')->name('release');
-    Route::get('/manager_home/scoredata/scoredata_norelease','Contactcontroller@norelease')->name('norelease');
-    Route::get('/home/scoredata/scoredata_register','Contactcontroller@scoredataRegister')->name('scoredata_register');
-    Route::post('/home/scoredata/scoredata_register','Contactcontroller@uplode')->name('uplode');
-    Route::get('/manager_home/scoredata/scoredata_detail','Contactcontroller@scoreDetail')->name('scoreDetail');
-    Route::get('/manager_home/scoredata/scoredata_edit','Contactcontroller@scoreedit')->name('scoreedit');
-    Route::post('/manager_home/scoredata/scoredata_edit','Contactcontroller@update')->name('update');
+    Route::get('/mng_register',[Contactcontroller::class, 'mngregister'])->name('mngregister');
+   // Route::get('/manager_login',[Contactcontroller::class,'managerLogin'])->name('manager_login');
+   // Route::get('/manager_home','[Contactcontroller::class,'managerHome'])->name('home');
+   // Route::get('/login/search','[Contactcontroller::class,'loginSearch'])->name('login_search');
+   // Route::post('/manager_home',[Contactcontroller::class,'mngloginComplete'])->name('home');
+    Route::get('/home/scoredata',[Contactcontroller::class,'scoreData'])->name('scoredata');
+    Route::get('/manager_home/scoredata_delete',[Contactcontroller::class,'delete'])->name('scoredelete');
+    Route::get('/manager_home/scoredata/scoredata_release',[Contactcontroller::class,'release'])->name('release');
+    Route::get('/manager_home/scoredata/scoredata_norelease',[Contactcontroller::class,'norelease'])->name('norelease');
+    Route::get('/home/scoredata/scoredata_register',[Contactcontroller::class,'scoredataRegister'])->name('scoredata_register');
+    Route::post('/home/scoredata/scoredata_register',[Contactcontroller::class,'uplode'])->name('uplode');
+    Route::get('/manager_home/scoredata/scoredata_detail',[Contactcontroller::class,'scoreDetail'])->name('scoreDetail');
+    Route::get('/manager_home/scoredata/scoredata_edit',[Contactcontroller::class,'scoreedit'])->name('scoreedit');
+    Route::post('/manager_home/scoredata/scoredata_edit',[Contactcontroller::class,'update'])->name('update');
 });
-//Route::post('/ajaxrelease','Contactcontroller@ajaxrelease')->name('posts.ajaxrelease');
-//Route::post('/ajaxnorelease','Contactcontroller@ajaxnorelease')->name('posts.ajaxnorelease');
+//Route::post('/ajaxrelease',[Contactcontroller::class,'ajaxrelease'])->name('posts.ajaxrelease');
+//Route::post('/ajaxnorelease',[Contactcontroller::class,'ajaxnorelease'])->name('posts.ajaxnorelease');
 
 
-Route::get('/login','Contactcontroller@login')->name('login');
-Route::get('/logout','Contactcontroller@logout')->name('logout');
+Route::get('/login',[Contactcontroller::class,'login'])->name('login');
+Route::get('/logout',[Contactcontroller::class,'logout'])->name('logout');
 
 Route::group(['middleware'=>['auth','can:user-higher']],function(){
-    Route::get('/tweet','Twittercontroller@tweet')->name('tweet');
-    Route::post('/login','Contactcontroller@loginComplete')->name('home');
-    Route::get('/register','Contactcontroller@register')->name('register');
-    Route::get('/login/search','Contactcontroller@userHome')->name('user_home');
-    Route::get('/login/record','Contactcontroller@recordpage')->name('record_page');
-    Route::get('/login/score','Contactcontroller@loginScorelink')->name('login_scorelink');
-    Route::get('/login/search','Contactcontroller@loginSearch')->name('login_search');
-    Route::get('/login/scoreedit','Contactcontroller@userScoreeditview')->name('user_score_edit_view');
-    Route::post('/login/scoreedit','Contactcontroller@userScoreedit')->name('user_score_edit');
-    Route::post('/login/search','Contactcontroller@loginSearch')->name('login_search');
-    Route::post('/register/complete','Contactcontroller@checkregister')->name('checkregister');
+    Route::get('/tweet',[Twittercontroller::class, 'tweet'])->name('tweet');
+    Route::post('/login',[Contactcontroller::class,'loginComplete'])->name('home');
+    Route::get('/register',[Contactcontroller::class,'register'])->name('register');
+    Route::get('/login/search',[Contactcontroller::class,'userHome'])->name('user_home');
+    Route::get('/login/record',[Contactcontroller::class,'recordpage'])->name('record_page');
+    Route::get('/login/score',[Contactcontroller::class,'loginScorelink'])->name('login_scorelink');
+    Route::get('/login/search',[Contactcontroller::class,'loginSearch'])->name('login_search');
+    Route::get('/login/scoreedit',[Contactcontroller::class,'userScoreeditview'])->name('user_score_edit_view');
+    Route::post('/login/scoreedit',[Contactcontroller::class,'userScoreedit'])->name('user_score_edit');
+    Route::post('/login/search',[Contactcontroller::class,'loginSearch'])->name('login_search');
+    Route::post('/register/complete',[Contactcontroller::class,'checkregister'])->name('checkregister');
 
 });
 
 //マイリスト
-Route::post('ajaxlike', 'Contactcontroller@ajaxlike')->name('posts.ajaxlike');
+Route::post('ajaxlike', [Contactcontroller::class,'ajaxlike'])->name('posts.ajaxlike');
 
 //パスワードリセット
-Route::get('/password', 'Contactcontroller@userPw')->name('user_pw');
+Route::get('/password', [Contactcontroller::class,'userPw'])->name('user_pw');
 Route::post('/password/reset', 'Contactcontroller@pwresetMail')->name('user_pw_urlmail');
 Route::get('/password/reset/newpassword', 'Contactcontroller@newpassword')->name('user_newpassword');
 Route::post('/password/reset/newpassword', 'Contactcontroller@update_password')->name('update_password');
